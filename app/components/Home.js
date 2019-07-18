bf2e69c4bb02cd8ff0f0cd16e33f3cda52bef164
fengbao <120254224@qq.com>
2019-07-18 16:07:56 +0800

    feat(dict): pronounce,google,like

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,10 +2,12 @@
 import React, { Component } from 'react';
 import styles from './Home.css';
 import IndexHeader from './Index/Header/Header';
-import { Breadcrumb, Collapse, Icon, Layout } from 'antd';
+import { Breadcrumb, Card, Col, Collapse, Icon, Layout, Rate, Row } from 'antd';
 import SideBar from './General/SideBar/SideBar';
 import AppFooter from './General/Footer/Footer';
 import { parserNames } from '../utils/config';
+import ReactAudioPlayer from 'react-audio-player';
+import { note as NoteService } from '../../app/services/note/NoteService';
 
 const { ipcRenderer } = require('electron');
 const { Content } = Layout;
@@ -22,19 +24,73 @@ export default class Home extends Component<Props> {
 
   constructor(props) {
     super(props);
-    ipcRenderer.on('search-results', (event, arg) => {
+    this.state = {
+      word: '',
+      result: [],
+      read: '',
+      score: 0
+    };
 
-      console.log('result received');
+    ipcRenderer.on('search-results', (event, arg) => {
+      //下次搜索是清空网络词典
       this.props.setResult(arg);
+      this.setState({ result: [] });
+
     });
+    ipcRenderer.on('search-word', (event, arg) => {
+      this.setState({ word: arg });
+    });
+
   }
+
+  setWord = (word) => {
+    this.setState({ word: word });
+    let favo = NoteService.find(word);
+    if (favo) {
+      this.setState({ score: favo.score });
+    } else {
+      this.setState({ score: 0 });
+
+    }
+  };
+
+  internet = () => {
+    ipcRenderer.send('translate-request', this.state.word);
+    ipcRenderer.on('translate-result', (event, arg) => {
+      let result;
+      if (arg.hasOwnProperty('dict')) {
+        result = arg.dict;
+      } else {
+        result = arg.result;
+      }
+      this.setState({
+        result
+      });
+    });
+
+  };
+
+  spell = () => {
+    ipcRenderer.send('read-request', this.state.word);
+    ipcRenderer.on('read-result', (event, arg) => {
+      let player = this.player.audioEl;
+      player.src = arg;
+      player.load();
+      player.play();
+    });
+  };
+
+  like = (value) => {
+    NoteService.add({ word: this.state.word, score: value });
+    this.setState({ score: value });
+  };
 
   render() {
 
     return (
       <Layout>
 
-        <IndexHeader {...this.props}/>
+        <IndexHeader setWord={this.setWord} {...this.props} />
         <Layout>
 
           <SideBar></SideBar>
@@ -44,6 +100,39 @@ export default class Home extends Component<Props> {
               <Breadcrumb.Item>List</Breadcrumb.Item>
               <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
+            <Card>
+              <Row>
+                <Col span={2}>
+                  <Icon type="audio" theme="twoTone" width='2em' height='2em' onClick={this.spell}/>
+                </Col>
+                <Col span={2}>
+                  <Icon type="compass" theme="twoTone" width={'20'} height='20' onClick={this.internet}/>
+                </Col>
+                <Col span={6}>
+                  <Rate character={<Icon type="heart"/>} style={{ color: 'red' }} onChange={this.like}
+                        value={this.state.score}/>
+                </Col>
+                <ReactAudioPlayer
+                  ref={element => {
+                    this.player = element;
+                  }}
+                  src=""
+                />
+              </Row>
+              <Row>
+                {this.state.result.length ?
+                  <Card>
+                    {
+                      this.state.result.map((item, index) => {
+                        return <p key={index}>{item}</p>;
+                      })
+                    }
+                  </Card>
+                  : ''
+                }
+              </Row>
+
+            </Card>
             <Content className={styles.content}
             >
               {Object.keys(this.props.dict.result) ? <Definitions result={this.props.dict.result}/> : ''}
@@ -79,8 +168,6 @@ const Definitions = ({ result }) => <Collapse
         <div dangerouslySetInnerHTML={{ __html: result[key] }}/>
 
       </Panel>;
-
-
     })}
 
 </Collapse>;
