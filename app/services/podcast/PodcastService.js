7a9c8fa718afb24f1dfbea3e8ff9d3354aad2ad9
fengbao <worldtongfb@gmail.com>
2020-04-12 16:40:22 +0800

    feat(podcast): podcast service

--- app/services/podcast/PodcastService.js
+++ app/services/podcast/PodcastService.js
@@ -4,18 +4,19 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const subscribeAdapter = new FileSync(resPath + '/databases/podcasts/subscribe.json');
 const subscribeDb = low(subscribeAdapter);
-
+const fs = require('fs');
+const xml2js = require('xml2js');
 import axios from 'axios';
 
-const fs = require('fs');
-
 class PodcastService {
+  parser = null;
   subscribe = (podcast) => {
     const { collectionId } = podcast;
     let exist = subscribeDb.get('podcasts').find({ collectionId }).value();
     if (exist) {
     } else {
       subscribeDb.get('podcasts').push(this.getDefault(podcast)).write();
+      this.sync(podcast);
 
     }
 
@@ -49,6 +50,24 @@ class PodcastService {
 
     return subscribeDb.get('podcasts').value();
   };
+
+  parseXML = (collectionId) => {
+
+    let parser = new xml2js.Parser();
+    let res = {};
+    let xmlStr = fs.readFileSync(`${resPath}/podcasts/subscribes/${collectionId}.xml`);
+
+
+    parser.parseString(xmlStr, function(err, result) {
+      res = result;
+    });
+    return res;
+  };
+
+  play = (item) => {
+
+  };
+
 
 }
 
