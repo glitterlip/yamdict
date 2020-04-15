e890d92f58ee97e91bcc72932e472b9b47a44e5b
glitterlip <worldtongfb@gmail.com>
2020-04-15 17:57:02 +0800

    fix(hook): iohook

--- app/boot.js
+++ app/boot.js
@@ -1,8 +1,9 @@
 import { registerTranslateEvent } from './utils/translate';
 import { registerConfig } from './utils/config';
-import {registerCustomeProtocol} from './services/app/CustomService';
+import { registerCustomeProtocol } from './services/app/CustomService';
+// import { registerIoHook } from './services/app/HookService';
 
 registerConfig();
 registerTranslateEvent();
 registerCustomeProtocol();
-
+// registerIoHook();
