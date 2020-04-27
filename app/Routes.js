9c2d81af8dd6b51677b6d3d9bff5b4a39f11be47
glitterlip <worldtongfb@gmail.com>
2020-04-27 10:59:15 +0800

    fix(component): extract header and footer

--- app/Routes.js
+++ app/Routes.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
@@ -10,19 +10,57 @@ import History from './containers/HistoryPage';
 import PodcastPage from './containers/PodcastPage';
 import DiscoverPage from './containers/podcast/DiscoverPage';
 import SubscribesPage from './containers/podcast/SubscribesPage';
+import IndexHeader from './components/Index/Header/Header';
+import { Layout } from 'antd';
+import SideBar from './components/General/SideBar/SideBar';
+import AppFooter from './components/General/Footer/Footer';
+import { defaultFunctions, defaultProps } from './reducers/index';
+import { connect } from 'react-redux';
 
-export default () => (
-  <App>
-    <Switch>
-      <Route path={routes.TRANSLATE.path} component={TranslatePage}/>
-      <Route path={routes.HOME.path} exact component={HomePage}/>
-      <Route path={routes.NOTE.path} exact component={NotePage}/>
-      <Route path={routes.DICTS.path} exact component={DictPage}/>
-      <Route path={routes.HISTORY.path} exact component={History}/>
-      <Route path={routes.PODCAST.path} exact component={PodcastPage}/>
-      <Route path={routes.PODCAST.path} exact component={PodcastPage}/>
-      <Route path='/podcast/discover' exact component={DiscoverPage}/>
-      <Route path='/podcast/subscribes' exact component={SubscribesPage}/>
-    </Switch>
-  </App>
-);
+
+function mapStateToProps(state) {
+  return { ...defaultProps(state) };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+
+  return { ...defaultFunctions(dispatch) };
+
+}
+
+class Routes extends Component<Props> {
+
+  props: Props;
+
+  render() {
+    return <App>
+      <Switch>
+        <React.Fragment>
+          <Layout>
+            <IndexHeader {...this.props}/>
+            <Layout>
+              <SideBar></SideBar>
+              <Route path={routes.TRANSLATE.path} component={TranslatePage}/>
+              <Route path={routes.HOME.path} exact component={HomePage}/>
+              <Route path={routes.NOTE.path} exact component={NotePage}/>
+              <Route path={routes.DICTS.path} exact component={DictPage}/>
+              <Route path={routes.HISTORY.path} exact component={History}/>
+              <Route path={routes.PODCAST.path} exact component={PodcastPage}/>
+              <Route path='/podcast/discover' exact component={DiscoverPage}/>
+              <Route path='/podcast/subscribes' exact component={SubscribesPage}/>
+            </Layout>
+            <AppFooter {...this.props}/>
+          </Layout>
+        </React.Fragment>
+
+      </Switch>
+    </App>;
+  };
+
+};
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Routes);
